852ebca7051279e733e2a0ca6bb8c1b22232a4ac
Marvin Schramm <marvin@kreait.com>
2021-03-09 21:10:13 +0100

    adds exception message tests

---
+++ src/test/kotlin/io/hndrs/jwt/UnauthenticatedUserExceptionTest.kt
@@ -0,0 +1,23 @@
+package io.hndrs.jwt
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+
+internal class UnauthenticatedUserExceptionTest {
+
+    @Test
+    fun nullMessage() {
+        val ex = UnauthenticatedUserException()
+        Assertions.assertEquals("401 UNAUTHORIZED \"Access denied: No message\"", ex.message)
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, ex.status)
+    }
+
+    @Test
+    fun providedMessage() {
+        val expectedMessage = "Exception Reason message"
+        val ex = UnauthenticatedUserException(expectedMessage)
+        Assertions.assertEquals("401 UNAUTHORIZED \"Access denied: $expectedMessage\"", ex.message)
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED, ex.status)
+    }
+}
