4e7e982024c7233a5459f5be8b892a47f0a94b41
Marvin Schramm <marvin@kreait.com>
2021-03-11 15:59:17 +0100

    add new gradle publishing info version

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
     id("idea")
     id("signing")
     jacoco
-    id("io.hndrs.publishing-info").version("1.1.0")
+    id("io.hndrs.publishing-info").version("2.0.0")
 }
 
 val isRelease = project.hasProperty("release")
